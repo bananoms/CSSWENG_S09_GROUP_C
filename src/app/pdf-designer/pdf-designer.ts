269cdfabe7894b4d7684741b5182581d01fc9512
rei-xier <edlynn_rei_encallado@dlsu.edu.ph>
2026-06-06 11:29:40 +0000

    pages working

---
+++ src/app/pdf-designer/pdf-designer.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pdf-designer',
+  imports: [],
+  templateUrl: './pdf-designer.html',
+  styleUrl: './pdf-designer.css',
+})
+export class PdfDesigner {}
