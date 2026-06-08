41ebf310b3323a5fd6822fe0b9738ac2ca6d59d1
ehcac <cabatay.chachi@gmail.com>
2026-06-08 13:45:09 +0800

    feat: backend structure changes

--- src/app/pdf-designer/pdf-designer.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-pdf-designer',
-  imports: [],
-  templateUrl: './pdf-designer.html',
-  styleUrl: './pdf-designer.css',
-})
-export class PdfDesigner {}
